7f2b60b1b49ef175bb7006000434bb28cfd2fcee
Conrad Bartels Daal <conrad@proxible.com>
2014-09-26 13:41:33 +0200

    Updated Framework imports
    Signed-off-by: Conrad Bartels Daal <conrad@proxible.com>

--- ProxibleMobileSDK.framework/Headers/ProxibleMobileSDK.h
+++ ProxibleMobileSDK.framework/Headers/ProxibleMobileSDK.h
@@ -14,6 +14,6 @@ FOUNDATION_EXPORT double ProxibleMobileSDKVersionNumber;
 //! Project version string for ProxibleMobileSDK.
 FOUNDATION_EXPORT const unsigned char ProxibleMobileSDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <ProxibleMobileSDK/PublicHeader.h>
+#import <ProxibleMobileSDK/PRXMobileSDK.h>
 
 
