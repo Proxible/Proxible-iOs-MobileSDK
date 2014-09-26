8038b47bcae32488907e6540ba17b7becb838fec
Conrad Bartels Daal <conrad@proxible.com>
2014-09-26 13:24:26 +0200

    Added framework
    Signed-off-by: Conrad Bartels Daal <conrad@proxible.com>

---
+++ ProxibleMobileSDK.framework/Headers/ProxibleMobileSDK.h
@@ -0,0 +1,19 @@
+//
+//  ProxibleMobileSDK.h
+//  ProxibleMobileSDK
+//
+//  Created by Conrad Bartels Daal on 9/26/14.
+//  Copyright (c) 2014 Proxible B.V. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProxibleMobileSDK.
+FOUNDATION_EXPORT double ProxibleMobileSDKVersionNumber;
+
+//! Project version string for ProxibleMobileSDK.
+FOUNDATION_EXPORT const unsigned char ProxibleMobileSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProxibleMobileSDK/PublicHeader.h>
+
+
